c17527925767e38495145ebf9f9530f00b7e8c85
will <wjandrews672@gmail.com>
2021-02-15 20:22:55 -0800

    added forEach loop with color change on hover

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const grid = document.querySelector('#border')
-const divAdd = document.createElement('div')
+const grid = document.querySelector('#border');
+const divAdd = document.createElement('div');
 // const div2 = divAdd.cloneNode(true);
 // const text = document.createTextNode('test');
 let divs = [];
@@ -8,6 +8,7 @@ let squares = 255;
 let fragment = document.createDocumentFragment();
 divAdd.classList.add('box');
 
+//why wont box variable work
 while (start < squares) {
     start++;
     divs.push(divAdd);
@@ -19,12 +20,20 @@ for (let i = 0; i <= divs.length; i++) {
     fragment.appendChild(divCln);
 };
 grid.appendChild(fragment);
+let boxes = document.querySelectorAll('.box');
+// function changeColor() {
+//     //grab box div and change background color
+//     box.style.backgroundColor = 'blue';
+// }
 //create 256 divs for 16x16 grid
 //create empty array that fills out with 256 divs
 //loop through array and append items to fragment
 //append fragment to DOM
-
-
+boxes.forEach(box => {
+    box.addEventListener('mouseover', () => {
+        box.style.backgroundColor = 'red';
+    });
+});
 
 
 
